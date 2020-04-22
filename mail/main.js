8eb102468ad5985c1cac1f7ae52c7fb5a7711994
Stefano <stefano2890@gmail.com>
2020-04-22 10:11:49 +0200

    mail complete

--- mail/main.js
+++ mail/main.js
@@ -4,3 +4,19 @@ console.log(mail_chart);
 // chiedere all'utente di inserire una mail
 var insertmail = prompt('Inserisci una mail valida: ');
 console.log(insertmail);
+// dichiaro variabile false
+var verify = false;
+// ciclo for per verificare ogni singolo elemento dell'array
+for (var i = 0; i < mail_chart.length; i++) {
+// se la mail inserita è uguale ad una di quelle inserite nell'array metto la variabile "verify = true"
+    if (insertmail == mail_chart[i]) {
+        verify = true;
+    }
+}
+// se verify è uguale a true stampa messaggio con accesso garantito
+if (verify == true) {
+    console.log('Accesso Garantito');
+// altrimenti stampa messaggio con accesso negato
+} else {
+    console.log('Accesso Negato');
+}
